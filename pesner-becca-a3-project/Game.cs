d6a571c7f04af1276f02c50fa2b90173f5fad3c9
Becca Pesner <000351580@mohawkcollege.ca>
2025-03-07 16:58:19 -0500

    adding jump and jump time

--- pesner-becca-a3-project/Game.cs
+++ pesner-becca-a3-project/Game.cs
@@ -1,6 +1,7 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
 using System.Numerics;
+using pesner_becca_a3_project;
 
 // The namespace your code is in.
 namespace MohawkGame2D
@@ -11,27 +12,59 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-        Texture2D capybara = Graphics.LoadTexture("./capybara.png");
+        Texture2D capybaraTexture = Graphics.LoadTexture("./capybara.png");
+        Vector2 capybaraPosition = new Vector2(0, 250);
+        float capybaraHeight = 150;
+        float jumpGrace = 5;
+        Vector2 groundStart = new Vector2(0, 450);
+        Vector2 groundSize = new Vector2(800, 150);
+        bool isJumping = false;
+        float jumpTimeDefault = 0.8f;
+        float jumpTime = 0;
+        float jumpPower = 1.5f;
+        float gravity = 1.7f;
+        Obstacle[] obstacles = [];
+        
+
         public void Setup()
         {
-        
-            Window.SetSize (800, 600);
+            Window.SetTitle("cappy blappy");
+            Window.SetSize(800, 600);
+            jumpTime = jumpTimeDefault;
         }
 
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
         public void Update()
         {
             Window.ClearBackground(Color.White);
             DrawGround();
-            Graphics.Draw(capybara, 0, 300);
+            Graphics.Draw(capybaraTexture, capybaraPosition);
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Space) && (groundStart.Y < capybaraPosition.Y + capybaraHeight + jumpGrace))
+            {
+                isJumping = true;
+            }
+            if (isJumping)
+            {
+                capybaraPosition += -Vector2.UnitY * jumpPower;
+                jumpTime -= Time.DeltaTime;
+                if (jumpTime < 0)
+                {
+                    jumpTime = jumpTimeDefault;
+                    isJumping = false;
+                }
+            }
+            else
+            {
+                if (groundStart.Y > capybaraPosition.Y + capybaraHeight)
+                {
+                    capybaraPosition += Vector2.UnitY * gravity;
+                }
+            }
         }
 
         public void DrawGround()
         {
             Draw.FillColor = new Color(70, 60, 10);
-            Draw.Rectangle(0, 450, 800, 150);
+            Draw.Rectangle(groundStart, groundSize);
         }
     }
 
---
+++ pesner-becca-a3-project/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pesner_becca_a3_project
+{
+    internal class Obstacle
+    {
+        float position = 0;
+
+        Obstacle()
+        {
+            position = 1000;
+        }
+        Obstacle(float p)
+        {
+            position = p;
+        }
+    }
+}
