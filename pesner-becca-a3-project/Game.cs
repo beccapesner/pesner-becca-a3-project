9ceceef76822b0a366e7e90f48ffcca5b5484905
Becca Pesner <000351580@mohawkcollege.ca>
2025-03-08 17:57:17 -0500

    adding game over text

--- pesner-becca-a3-project/Game.cs
+++ pesner-becca-a3-project/Game.cs
@@ -1,5 +1,7 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Numerics;
 using pesner_becca_a3_project;
 using Raylib_cs;
@@ -25,21 +27,42 @@ namespace MohawkGame2D
         float jumpTime = 0;
         float jumpPower = 1.5f;
         float gravity = 1.7f;
-        Obstacle[] obstacles = [new Obstacle(), new Obstacle(1100)];
-        
+        float obstacleSpeed = 3.0f;  // speed of obstacles
+        float score = 0f;  // score based on time
 
+        Obstacle[] obstacles = [new Obstacle(), new Obstacle(1100)];
+
+        //Game state management
+        bool isGameOver = false;
         public void Setup()
         {
             Window.SetTitle("cappy blappy");
             Window.SetSize(800, 600);
             jumpTime = jumpTimeDefault;
+            isGameOver = false;
         }
 
         public void Update()
         {
+            if (isGameOver)
+            {
+                GameOver();
+            }
+            else
+            {
+                PlayGame();
+            }
+        }
+
+        // when the game is running 
+        public void PlayGame()
+
+        {
             Window.ClearBackground(Color.White);
             DrawGround();
             Graphics.Draw(capybaraTexture, capybaraPosition);
+
+            // player jump logic 
             if (Input.IsKeyboardKeyPressed(KeyboardInput.Space) && (groundStart.Y < capybaraPosition.Y + capybaraHeight + jumpGrace))
             {
                 isJumping = true;
@@ -61,6 +84,39 @@ namespace MohawkGame2D
                     capybaraPosition += Vector2.UnitY * gravity;
                 }
             }
+
+            // check for game-over condition (e.g., collision with obstacle)
+            foreach (var obstacle in obstacles)
+            {
+                if (capybaraPosition.X + capybaraHeight > obstacle.Position.X && capybaraPosition.X < obstacle.Position.X + obstacle.Width)
+                {
+                    isGameOver = true;
+                }
+            }
+        }
+
+        // logic when the game is over
+        public void GameOver()
+        {
+            Window.ClearBackground(Color.Red); // change the background to red when game is over and put "game over" text
+            Text.Color = Color.White;
+            Text.Size = 7;
+            Text.Draw("game over!", 300, 250);
+            // reset the game after some time or on key press enter
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Enter))
+            {
+                ResetGame();
+            }
+        }
+
+        // resets the game state
+        public void ResetGame()
+        {
+            capybaraPosition = new Vector2(0, 250);
+            isJumping = false;
+            isGameOver = false;
+            jumpTime = jumpTimeDefault;
+            obstacles = new Obstacle[] { new Obstacle(), new Obstacle(1100) }; // reset obstacles
         }
 
         public void DrawGround()
